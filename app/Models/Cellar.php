2c3346204184cc632d28af51f5cbf71cd3162290
Mateo <mateofortin@gmail.com>
2025-01-04 17:14:11 -0500

    Modified Cellar Model

--- app/Models/Cellar.php
+++ app/Models/Cellar.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Cellar extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id',
+    ];
 }
