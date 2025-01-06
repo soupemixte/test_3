46c4a08dc2b8c142ec5387828359097c6235fd22
Mateo <mateofortin@gmail.com>
2025-01-06 13:15:41 -0500

    So much CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
             @auth
                 @if(Auth::user()->isAdmin)
                 <li>@lang('lang.bottles')
-                <ul>
+                <ul class="dropdown">
                     <li><a href="{{ route('bottle.delete') }}">@lang('lang.delete_bottle')</a></li>
                     <li><a href="{{ route('bottle.scrape') }}">@lang('lang.scrape_bottle')</a></li>
                 </ul></li>
@@ -35,18 +35,13 @@
                 <li><a class="nav-link" href="{{ route('logout') }}">@lang('lang.logout')</a></li>
             @endguest
                 <li>@lang('lang.lang')
-                <ul>
-                    <li><a class="nav-link" href="{{ route('lang', 'en') }}">@lang('lang.lang_en')</a></li>
-                    <li><a class="nav-link" href="{{ route('lang', 'fr') }}">@lang('lang.lang_fr')</a></li>
+                <ul class="dropdown hidden">
+                    <li><a class="" href="{{ route('lang', 'en') }}">@lang('lang.lang_en')</a></li>
+                    <li><a class="" href="{{ route('lang', 'fr') }}">@lang('lang.lang_fr')</a></li>
                 </ul></li>
             </ul>
         </nav>
     </footer>
-    <!-- <nav class="navigation">
-      <a class="nav-link" href="/"> <img src="{{asset('img/navigation/home.svg') }}" alt="nav-image">@lang('lang.home')</a>
-      <a class="nav-link" href="{{ route('cellar.index') }}"> <img src="{{asset('img/navigation/my-collection.svg') }}" alt="nav-image">@lang('lang.cellars')</a>
-      <a class="nav-link" href="{{ route('bottle.index') }}"> <img src="{{asset('img/navigation/catalog.svg') }}" alt="nav-image">@lang('lang.bottles')</a>
-      <a class="nav-link" href="{{ route('user.show') }}"> <img src="{{asset('img/navigation/profile.svg') }}" alt="nav-image">@lang('lang.profile')</a>
-    </nav> -->
+
 </body>
 </html>
