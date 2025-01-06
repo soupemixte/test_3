9a57e3c08fda26ea913b6ae5dd541ac10ada77ea
Mateo <mateofortin@gmail.com>
2025-01-06 11:42:35 -0500

    More CSS for header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,17 +11,22 @@
 </head>
 <body>
     <!-- Header -->
-    <header class="header">
-        <div class="logo">VINO</div>
+    <header>
+        <div class="logo"><h1>VINO</h1></div>
+    </header>
+    <!-- Content -->
+    @yield('content')
+    <!-- Navigation -->
+    <footer>
         <nav>
             <ul>
             @auth
                 @if(Auth::user()->isAdmin)
                 <li>@lang('lang.bottles')
-                    <ul>
-                        <li><a href="{{ route('bottle.delete') }}">@lang('lang.delete_bottle')</a></li>
-                        <li><a href="{{ route('bottle.scrape') }}">@lang('lang.scrape_bottle')</a></li>
-                    </ul></li>
+                <ul>
+                    <li><a href="{{ route('bottle.delete') }}">@lang('lang.delete_bottle')</a></li>
+                    <li><a href="{{ route('bottle.scrape') }}">@lang('lang.scrape_bottle')</a></li>
+                </ul></li>
                 @endif
             @endauth
             @guest
@@ -30,21 +35,13 @@
                 <li><a class="nav-link" href="{{ route('logout') }}">@lang('lang.logout')</a></li>
             @endguest
                 <li>@lang('lang.lang')
-                    <ul>
-                        <li><a class="nav-link" href="{{ route('lang', 'en') }}">@lang('lang.lang_en')</a></li>
-                        <li><a class="nav-link" href="{{ route('lang', 'fr') }}">@lang('lang.lang_fr')</a></li>
-                    </ul></li>
+                <ul>
+                    <li><a class="nav-link" href="{{ route('lang', 'en') }}">@lang('lang.lang_en')</a></li>
+                    <li><a class="nav-link" href="{{ route('lang', 'fr') }}">@lang('lang.lang_fr')</a></li>
+                </ul></li>
             </ul>
         </nav>
-
-        
-            
-        
-
-    </header>
-    <!-- Content -->
-    @yield('content')
-    <!-- Navigation -->
+    </footer>
     <!-- <nav class="navigation">
       <a class="nav-link" href="/"> <img src="{{asset('img/navigation/home.svg') }}" alt="nav-image">@lang('lang.home')</a>
       <a class="nav-link" href="{{ route('cellar.index') }}"> <img src="{{asset('img/navigation/my-collection.svg') }}" alt="nav-image">@lang('lang.cellars')</a>
