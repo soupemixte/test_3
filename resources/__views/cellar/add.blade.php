6563719a4afa251b42487270c7bb4b12f4b2bf30
Mateo <mateofortin@gmail.com>
2025-01-06 10:14:55 -0500

    Renamed folders and files to start doing css

---
+++ resources/__views/cellar/add.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+@section('title', 'Ajouter la bouteille')
+@section('content')
+<main class="flex-center">
+    <section class="structure flex-col-center height80">   
+    <form class="form" action="{{ route('cellar.storeBottle') }}" method="POST">
+        @csrf
+        <div class="form-control">
+            <label for="bottle_name">Nom de la Bouteille</label>
+            <input type="text" name="bottle_name" value="{{ $bottle->title }}" readonly>
+            @if ($errors->has('bottle_name'))
+                <div class="error-message">
+                    {{ $errors->first('bottle_name') }}
+                </div>
+            @endif
+        </div>
+
+        <div class="form-control">
+            <label for="cellar_id">Choisir le Cellier</label>
+            <select name="cellar_id" id="cellar_id" required>
+                <option value="">Choisir le Nom</option>
+                
+                @if (Auth::user()->cellars && Auth::user()->cellars->count())
+                    @foreach (Auth::user()->cellars as $cellar)
+                        <option value="{{ $cellar->id }}">{{ $cellar->name }}</option>
+                    @endforeach
+                @else
+                    <option value="" disabled>Aucun cellier disponible</option>
+                @endif
+            </select>
+            @if ($errors->has('cellar_id'))
+                <div class="error-message">
+                    {{ $errors->first('cellar_id') }}
+                </div>
+            @endif
+        </div>
+
+        <button type="submit" class="btn-border">Ajouter la bouteille</button>
+    </form>
+
+    </section>
+</main>
+
+@endsection
