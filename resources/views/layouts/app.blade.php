8cc472e4d5e33fdc132348c4d1c96d763f51136a
Mateo <mateofortin@gmail.com>
2025-01-06 10:35:55 -0500

    Moved css files things around

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/styles.css')}}" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
-    <meta name="author" content="Mateo">
-    <meta name="author" content="Dionis">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"> -->
+    <meta name="author" content="Equipe #1">
     <script type="module" src="{{ asset('js/main.js')}}" defer></script>
     <title>{{ config('app.name') }} - @yield('title')</title>
 </head>
