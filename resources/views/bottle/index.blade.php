047e763a78f7a0812a3d855fba19d5c1677fbcdf
Dionis <e2396498@cmaisonneuve.qc.ca>
2025-01-15 17:14:07 -0500

    updating the icon logic to delete or submit the search

--- resources/views/bottle/index.blade.php
+++ resources/views/bottle/index.blade.php
@@ -1,30 +1,43 @@
 @extends('layouts.app')
 @section('title', 'View all Bottles')
 @section('content')
-<main class="flex-center">    
+<main class="flex-center height80">    
         <div class="structure">
             <!-- <div class="search-box">
                 <button class="btn-search"><i class="fas fa-search"></i></button>
                 <input type="text" class="input-search" placeholder="Type to Search...">
             </div> -->
             <header class="filter-wrapper">
-                <form action="{{ route('bottle.index') }}" method="GET" class="search-container">
+                <form action="{{ route('bottle.index') }}" method="GET" class="search-container" id="search-form">
                     <input 
                         type="text" 
                         name="search" 
                         placeholder="Search..." 
                         class="search-input"
                         value="{{ old('search', $query ?? '')}}"
+                        id="search-input"
                     >
-                    <button type="submit" class="search-btn">
-                        <i class="fas fa-search"></i>
+                    <button type="submit" class="search-btn" id="search-btn">
+                        <i class="fas fa-search" id="search-icon"></i>
+                        <!-- <i class="fa fa-window-close"></i> -->
                     </button>
                    
                 </form>
             </header>
             <section class="grid">
                 @if ($bottles->isEmpty())
-                    <p>Aucune bouteille disponible.</p>
+                    <div class="no_results">
+                        @if (!empty($query))
+                            <h2>Recherche de : "{{ $query }}"</h2>
+                        @endif
+                        <span>0 résultats trouvés</span>
+                        <ul>Désolé, aucun résultat trouvé.
+                            <li>Essayez une autre recherche</li>
+                            <li>Ou retourner à la page de la liste des bouteilles</li>
+                        </ul>
+                        <a href="{{ route('bottle.index') }}" class="btn">@lang('lang.bottles')</a>
+
+                    </div>
                 @else
                 @foreach ($bottles as $bottle)
                     <article class="card_bottle">
@@ -55,5 +68,8 @@
             </section>
         </div>
 </main>
+
+<!---Change the icon of the search box--->
+
 
 @endsection
