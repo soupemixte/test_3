0707cfacfcbf05e906384e627c96d75395332f21
Mateo <mateofortin@gmail.com>
2025-01-16 20:54:54 -0500

    I'm done with the methods for add bottle and remove bottle

--- app/Http/Controllers/CellarController.php
+++ app/Http/Controllers/CellarController.php
@@ -70,8 +70,10 @@ class CellarController extends Controller
         }
 
         if (Auth::user()->hasCellar()) {
+            // return $cellar->id;
             // Redirect to the form to choose a cellar and add the bottle
             // return view('cellar.add', compact('bottle'));
+            
                 return view('cellar.show', [
                     'cellar' => $cellar,
                     'bottles' => $bottles,
@@ -118,8 +120,27 @@ class CellarController extends Controller
 
         // Check if the user has any cellars
         if (Auth::user()->hasCellar()) {
+            // return Auth::user()->id;
+            $cellar = Cellar::where('user_id', Auth::user()->id);
+            $cellar_id = $cellar->first()->id;
+            // return $cellar_id;
+            $cellar_bottle = CellarBottle::select()
+            ->where('cellar_id', '=', $cellar_id)
+            ->where('bottle_id', '=', $id)
+            ->exists();
+            // return $cellar_bottle;
+            if($cellar_bottle) 
+            { 
+                $cellar_bottle = CellarBottle::select()
+                    ->where('cellar_id', '=', $cellar_id)
+                    ->where('bottle_id', '=', $id)
+                    ->get();
+                $bd_quantity = $cellar_bottle->first()->quantity; 
+            }
+            else { $bd_quantity = 0; }
+            // return $bd_quantity;
             // Redirect to the form to choose a cellar and add the bottle
-            return view('cellar.add', compact('bottle'));
+            return view('cellar.add', compact('bottle'), ['quantity' => $bd_quantity]);
         }
 
         // If the user has no cellars, redirect to create a new cellar
@@ -140,15 +161,21 @@ class CellarController extends Controller
         $cellar_bottle = CellarBottle::select()
             ->where('cellar_id', '=', $request->input('cellar_id'))
             ->where('bottle_id', '=', $request->input('bottle_id'))
-            ->get();
+            ->exists();
+        
         
         if($cellar_bottle) {
+            $cellar_bottle = CellarBottle::select()
+            ->where('cellar_id', '=', $request->input('cellar_id'))
+            ->where('bottle_id', '=', $request->input('bottle_id'))
+            ->get();
             // FIXME:
             $bd_quantity = $cellar_bottle->first()->quantity;
             $set_quantity = $bd_quantity + $request->input('quantity');
 
             if($set_quantity < 0) {
-                return "Not enough bottles in cellar.";
+                // return "Not enough bottles in cellar."
+                return view('cellar.add', compact('bottle'), ['quantity' => $request->input('quantity')])->with('warning', 'Pas assez de Bouteilles dans le Cellier');
             }
             
             CellarBottle::where('cellar_id', '=', $request->input('cellar_id'))
@@ -160,7 +187,8 @@ class CellarController extends Controller
         else {
 
             if($request->input('quantity') < 0) {
-                return "Cannot add negative number";
+                // return "Cannot add negative number";
+                return view('cellar.add', compact('bottle'), ['quantity' => $request->input('quantity')])->with('warning', 'Erreur de gestion de cellier');
             }
             // Attach the bottle to the selected cellar
             CellarBottle::create([
