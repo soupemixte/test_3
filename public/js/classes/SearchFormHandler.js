954e433937b4e8d524b7855c1d819e7bdb6da249
Dionis <e2396498@cmaisonneuve.qc.ca>
2025-01-25 21:55:42 -0500

    reformating the validation for the filtering

--- public/js/classes/SearchFormHandler.js
+++ public/js/classes/SearchFormHandler.js
@@ -1,8 +1,11 @@
 class SearchFormHandler {
-    constructor(formId, inputId, buttonId) {
+    constructor(formId, inputId, buttonId, filterSelectors) {
         this.searchForm = document.getElementById(formId);
         this.searchInput = document.getElementById(inputId);
         this.searchBtn = document.getElementById(buttonId);
+        this.filterSelectors = filterSelectors.map((selector) =>
+            document.getElementById(selector)
+        );
 
         this.initializeEvents();
     }
@@ -10,21 +13,40 @@ class SearchFormHandler {
     initializeEvents() {
         // Prevent form submission if input is empty
         this.searchForm.addEventListener("submit", (e) => {
-            if (this.searchInput.value.trim() === "") {
+            if (
+                this.searchInput.value.trim() === "" &&
+                !this.isAnyFilterSelected()
+            ) {
                 e.preventDefault();
+                alert(
+                    "Veuillez entrer un terme de recherche ou choisir un filtre."
+                );
             }
         });
 
         // Handle click on the search button to focus on the input if empty
         this.searchBtn.addEventListener("click", (e) => {
-            if (this.searchInput.value.trim() === "") {
+            if (
+                this.searchInput.value.trim() === "" &&
+                !this.isAnyFilterSelected()
+            ) {
                 e.preventDefault();
                 this.searchInput.focus();
             }
         });
     }
+    // Check if any filter option is selected
+    isAnyFilterSelected() {
+        return this.filterSelectors.some(
+            (filter) => filter.value.trim() !== ""
+        );
+    }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    new SearchFormHandler("search-form", "search-input", "search-btn");
+    new SearchFormHandler("search-form", "search-input", "search-btn", [
+        "color",
+        "country",
+        "size",
+    ]);
 });
