b93e1f4df73571f4f6727aa1e646c73b56a7cc6d
DavidJ97 <davidjules97@gmail.com>
2025-01-27 15:31:50 -0500

    style: mise à jour des couleurs des cartes et des filtres

--- app/Http/Controllers/BottleController.php
+++ app/Http/Controllers/BottleController.php
@@ -18,56 +18,49 @@ class BottleController extends Controller
      * Display a listing of the resource.
      */
     public function index(Request $request)
-{
-    // Retrieve filter values
-    
-    $colors = Bottle::select('color')->distinct()->pluck('color');
-    $countries = Bottle::select('country')->distinct()->pluck('country');
-    $sizes = Bottle::select('size')->distinct()->pluck('size');
+    {
+        // Retrieve filter values
+        $colors = Bottle::select('color')->distinct()->pluck('color');
+        $countries = Bottle::select('country')->distinct()->pluck('country');
+        $sizes = Bottle::select('size')->distinct()->pluck('size');
 
-    $query = $request->input('search');
-    $filter = $request->input('order');
-    $color = $request->input('color');
-    $country = $request->input('country');
-    $size = $request->input('size');
-   
-    // Base query
-    $bottlesQuery = Bottle::query();
+        $query = $request->input('search');
+        $filter = $request->input('order');
+        $color = $request->input('color');
+        $country = $request->input('country');
+        $size = $request->input('size');
+       
+        // Base query
+        $bottlesQuery = Bottle::query();
 
-    // Apply filters if they exist
-    if ($query) {
-        $bottlesQuery->where('title', 'LIKE', '%' . $query . '%');
+        // Apply filters if they exist
+        if ($query) {
+            $bottlesQuery->where('title', 'LIKE', '%' . $query . '%');
+        }
+
+        if ($color) {
+            $bottlesQuery->where('color', $color);
+        }
+
+        if ($country) {
+            $bottlesQuery->where('country', $country);
+        }
+
+        if ($size) {
+            $bottlesQuery->where('size', $size);
+        }
+
+        if ($filter) {
+            $order = $filter;
+        } else {
+            $order = 'title';
+        }
+        // Get filtered results
+        $bottles = $bottlesQuery->orderby('title')->paginate(5);
+
+        // Pass data to the view
+        return view('bottle.index', compact('bottles', 'order', 'query', 'colors', 'countries', 'sizes', 'color', 'country', 'size'));
     }
-
-    if ($color) {
-        $bottlesQuery->where('color', $color);
-    }
-
-    if ($country) {
-        $bottlesQuery->where('country', $country);
-    }
-
-    if ($size) {
-        $bottlesQuery->where('size', $size);
-    }
-
-    if ($filter) {
-        $order = $filter;
-    } else {
-        $order = 'title';
-    }
-    // Get filtered results
-    $bottles = $bottlesQuery->orderby('title')->paginate(5);
-
-<<<<<<< HEAD
-// Pass data to the view
-return view('bottle.index', compact('bottles', 'order', 'query', 'colors', 'countries', 'sizes', 'color', 'country', 'size'));
-=======
-    // Pass data to the view
-    return view('bottle.index', compact('bottles', 'order', 'query', 'colors', 'countries', 'sizes', 'color', 'country', 'size'));
->>>>>>> e177bd1e1548cacec90fdb1ec445332c7c0f968b
-}
-
 
     public function details($id)
     {
@@ -76,10 +69,8 @@ return view('bottle.index', compact('bottles', 'order', 'query', 'colors', 'coun
         $celliers = Cellar::all();
         // Pass the bottles to the view
         return view('bottle.details', ['bottle'=>$bottle,'celliers'=>$celliers]);
-    
     }
     
-
     /**
      * Lance le scraping des bouteilles depuis le site de la SAQ en parcourant plusieurs pages.
      */
@@ -96,21 +87,16 @@ return view('bottle.index', compact('bottles', 'order', 'query', 'colors', 'coun
         // Reset the "scraping" flag
         Cache::forget('scraping');
         if (!Cache::get('scraping')) {
-            //
             return response()->json(['success' => true, 'message' => 'Extraction arrêtée par l\'utilisateur.']);
         }
-        //
         return response()->json(['success' => true, 'message' => 'Extraction terminée avec succès !']);
     }
-
 
     public function stopScraping() {
         // Set the scraping flag to false
         Cache::put('scraping', false);
-        //
         return response()->json(['success' => true, 'message' => 'Extraction arrêtée avec succès !']);
     }
-
 
     /**
      * Scrape les titres des bouteilles sur une page et gère la pagination.
@@ -167,19 +153,14 @@ return view('bottle.index', compact('bottles', 'order', 'query', 'colors', 'coun
         }
     }
 
-
     /**
      * Extract data by "data-th" attribute from the crawler.
-     * @param string $field The label to search for (e.g., 'Pays', 'Région').
-     * @return string The extracted text or 'N/A' if not found.
      */
     private function extractData($crawler, $field) 
     {
-        // Selector to find data-th attribute
         $selector = 'ul.list-attributs li strong[data-th="' . $field . '"]';
         $element = $crawler->filter($selector);
         
-        // Check if the element exists and return the text
         if ($element->count() > 0){
             return trim($element->text());
         } else {
@@ -187,42 +168,26 @@ return view('bottle.index', compact('bottles', 'order', 'query', 'colors', 'coun
         }
     }
 
-
     /**
      * Scrape detailed information for a specific wine bottle from its SAQ page.
      */
     private function scrapeBouteilleDetails($url, $client) {
         $crawler = $client->request('GET', $url);
 
-        // Extract details using the helper function
-        $saqCode = $this->extractData($crawler, 'Code SAQ');
-        $country = $this->extractData($crawler, 'Pays');
-        $region = $this->extractData($crawler, 'Région');
-        $designationOfOrigin = $this->extractData($crawler, "Appellation d'origine");
-        $classification = $this->extractData($crawler, 'Classification');
-        $grapeVariety = $this->extractData($crawler, 'Cépage');
-        $degreeAlcohol = $this->extractData($crawler, "Degré d'alcool");
-        $sugarContent = $this->extractData($crawler, 'Taux de sucre');
-        $color = $this->extractData($crawler, 'Couleur');
-        $particularity = $this->extractData($crawler, 'Particularité');
-        $size = $this->extractData($crawler, 'Format');
-        $producer = $this->extractData($crawler, 'Producteur');
-        $promotingAgent = $this->extractData($crawler, 'Agent promotionnel');
-
         return [
-            'saq_code' => $saqCode,
-            'country' => $country,
-            'region' => $region,
-            'designation_of_origin' => $designationOfOrigin,
-            'classification' => $classification,
-            'grape_variety' => $grapeVariety,
-            'degree_alcohol' => $degreeAlcohol,
-            'sugar_content' => $sugarContent,
-            'color' => $color,
-            'particularity' => $particularity,
-            'size' => $size,
-            'producer' => $producer,
-            'promoting_agent' => $promotingAgent,
+            'saq_code' => $this->extractData($crawler, 'Code SAQ'),
+            'country' => $this->extractData($crawler, 'Pays'),
+            'region' => $this->extractData($crawler, 'Région'),
+            'designation_of_origin' => $this->extractData($crawler, "Appellation d'origine"),
+            'classification' => $this->extractData($crawler, 'Classification'),
+            'grape_variety' => $this->extractData($crawler, 'Cépage'),
+            'degree_alcohol' => $this->extractData($crawler, "Degré d'alcool"),
+            'sugar_content' => $this->extractData($crawler, 'Taux de sucre'),
+            'color' => $this->extractData($crawler, 'Couleur'),
+            'particularity' => $this->extractData($crawler, 'Particularité'),
+            'size' => $this->extractData($crawler, 'Format'),
+            'producer' => $this->extractData($crawler, 'Producteur'),
+            'promoting_agent' => $this->extractData($crawler, 'Agent promotionnel'),
         ];
     }
 
@@ -231,17 +196,11 @@ return view('bottle.index', compact('bottles', 'order', 'query', 'colors', 'coun
      */
     public function getTotalBottles()
     {
-        $totalBottles = Bottle::count();
-        // Return the total bottle count as JSON
         return response()->json(['totalBottles' => Bottle::count()]);
     }
 
-
-
     public function destroy() {
-        $delete = Bottle::truncate();
-
+        Bottle::truncate();
         return view('welcome');
     }
 }
-
