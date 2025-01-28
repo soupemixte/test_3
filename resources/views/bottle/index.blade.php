a06535fed4afa85e53b8da44410969b7c241f922
Dionis <e2396498@cmaisonneuve.qc.ca>
2025-01-28 16:58:06 -0500

    adding the future list form button

--- resources/views/bottle/index.blade.php
+++ resources/views/bottle/index.blade.php
@@ -108,6 +108,10 @@
                         <picture>
                             <img src="{{ $bottle->image_src ?? asset('img/gallery/bottle_1.webp') }}" alt="{{ $bottle->title }}">
                         </picture>
+                        <form action="" class="cart">
+                            <button type="submit" class="btn-icon"><i class="fa-solid fa-cart-shopping"></i></button>
+                        </form>
+                        
                         <div class="card-body">
                             <div class="card-title">
                                 <h2>
@@ -124,8 +128,8 @@
                             <div>
                         </div>
                         <div class="btn-container flex-center gap5">
-                            <a href="{{ route('bottle.details', ['id' => $bottle->id]) }}" class="btn-border">@lang('lang.view')<i class="fa-solid fa-eye"></i></a>
-                            <a href="{{ route('cellar.add', ['id' => $bottle->id]) }}" class="btn-border btn-go"><i class="fa-solid fa-plus"></i></a>
+                            <a href="{{ route('bottle.details', ['id' => $bottle->id]) }}" class="btn-icon">@lang('lang.view')<i class="fa-solid fa-eye"></i></a>
+                            <a href="{{ route('cellar.add', ['id' => $bottle->id]) }}" class="btn-icon btn-go">Ajouter<i class="fa-solid fa-plus"></i></a>
                         </div>
                     </article>
                 @endforeach
