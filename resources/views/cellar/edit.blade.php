024568ce759fd5bd2af84f068d091d8c8111b475
Dionis <e2396498@cmaisonneuve.qc.ca>
2025-01-29 09:31:05 -0500

    add the confirmation popup for the cellar/edit page

--- resources/views/cellar/edit.blade.php
+++ resources/views/cellar/edit.blade.php
@@ -10,30 +10,57 @@
                 <label for="title">@lang('lang.cellar_name')</label>
                 <input type="text" name="title" value="{{ old('title', $cellar->title) }}" placeholder="Entrez le nom...">
                 @if ($errors->has('title'))
-                    <div class="alert_msg">
-                        {{$errors->first('title')}}
-                    </div>
+                <div class="alert_msg">
+                    {{$errors->first('title')}}
+                </div>
                 @endif
             </div>
             <div class="form-control">
                 <label for="description">@lang('lang.cellar_desc')</label>
                 <textarea name="description" placeholder="Description de ce cellier....">{{ old('description', $cellar->description) }}</textarea>
                 @if ($errors->has('description'))
-                    <div class="alert_msg">
-                        {{$errors->first('description')}}
-                    </div>
+                <div class="alert_msg">
+                    {{$errors->first('description')}}
+                </div>
                 @endif
             </div>
             <button type="submit" class="btn-border btn-icon btn-go flex-al just-between gap5">@lang('lang.cellar_create')<i class="fa-solid fa-pen-to-square"></i></button>
         </form>
         @if(Auth::id() == $cellar->user_id)
-        <form method="POST" action="{{ route('cellar.destroy', $cellar->id) }}">
-            @csrf
-            @method('delete')
-            <button type="submit" class="btn-border btn-icon btn-remove flex-al just-between gap5">supprimer<i class="fa-solid fa-trash"></i></button>
-        </form>
+        <button type="submit" class="btn-border btn-icon btn-remove flex-al just-between gap5" id="delete-btn">supprimer<i class="fa-solid fa-trash"></i></button>
+        <div class="popup-overlay hide" id="popup-overlay">
+            <div class="popup-delete">
+                <div class="message">
+                    <h2>Etes-vous sûr de vouloir supprimer ?</h2>
+                </div>
+                <div class="confirm-buttons">
+                    <button id="cancel-btn">Annuler <i class="fa-solid fa-ban"></i></button>
+                    <form method="POST" action="{{ route('cellar.destroy', $cellar->id) }}">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn-border btn-icon btn-remove flex-al just-between gap5">supprimer<i class="fa-solid fa-trash"></i></button>
+                    </form>
+                </div>
+            </div>
+        </div>
         @endif
     </section>
 </main>
 
+
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("#delete-btn").forEach(deleteBtn => {
+        const popupOverlay = document.querySelector(".popup-overlay");
+
+        deleteBtn.addEventListener("click", function () {
+            popupOverlay.classList.remove("hide");
+        });
+
+        popupOverlay.querySelector("#cancel-btn").addEventListener("click", function () {
+            popupOverlay.classList.add("hide"); 
+        });
+    });
+});
+</script>
 @endsection
