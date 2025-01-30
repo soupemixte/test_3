2a38bb9f508f9e135f6d91e807b685a95da7b138
DavidJ97 <davidjules97@gmail.com>
2025-01-29 19:54:53 -0500

    Correction d'une petite erreur

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -10,7 +10,7 @@
 <main class="flex-center height60">
     <section class="structure flex-col gap10">
         <div class="section-title">
-            <h2>Profil d'usager.</h2>
+            <h2>Profil d'usager</h2>
         </div>
         <div class="line"></div> 
         <div class="info-details profile">
